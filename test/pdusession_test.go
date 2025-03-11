8fd6f6a122b2c421748a5649939baa1d3705509f
ian-cs12-NYCU <ian.cs12@nycu.edu.tw>
2025-03-11 14:42:27 +0000

    feat: initial testbed

---
+++ test/pdusession_test.go
@@ -0,0 +1,251 @@
+package test_test
+
+import (
+	"fmt"
+	"net"
+	"testing"
+	"time"
+
+	"test"
+	"test/consumerTestdata/UDM/TestGenAuthData"
+	"test/nasTestpacket"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/free5gc/nas"
+	"github.com/free5gc/nas/nasMessage"
+	"github.com/free5gc/nas/nasType"
+	"github.com/free5gc/nas/security"
+	"github.com/free5gc/ngap"
+	"github.com/free5gc/openapi/models"
+)
+
+// TODO: After establishing the PDU session, you can send data over this PDU session
+func TestPDUSessionSendData(t *testing.T) {
+	var n int
+	var sendMsg []byte
+	var recvMsg = make([]byte, 2048)
+
+	// RAN connect to AMF
+	conn, err := test.ConnectToAmf(amfN2Ipv4Addr, ranN2Ipv4Addr, 38412, 9487)
+	defer conn.Close()
+	assert.Nil(t, err)
+
+	// send NGSetupRequest Msg
+	sendMsg, err = test.GetNGSetupRequest([]byte("\x00\x01\x02"), 24, "free5gc")
+	assert.Nil(t, err)
+	_, err = conn.Write(sendMsg)
+	assert.Nil(t, err)
+
+	// receive NGSetupResponse Msg
+	n, err = conn.Read(recvMsg)
+	assert.Nil(t, err)
+	_, err = ngap.Decoder(recvMsg[:n])
+	assert.Nil(t, err)
+
+	// New UE
+	ue := test.NewRanUeContext("imsi-2089300007487", 1, security.AlgCiphering128NEA0, security.AlgIntegrity128NIA2,
+		models.AccessType__3_GPP_ACCESS)
+	ue.AmfUeNgapId = 1
+	ue.AuthenticationSubs = test.GetAuthSubscription(TestGenAuthData.MilenageTestSet19.K,
+		TestGenAuthData.MilenageTestSet19.OPC,
+		TestGenAuthData.MilenageTestSet19.OP)
+	servingPlmnId := "20893"
+
+	// insert UE data to MongoDB
+	test.InsertUeToMongoDB(t, ue, servingPlmnId)
+
+	// send InitialUeMessage(Registration Request)(imsi-2089300007487)
+	mobileIdentity5GS := nasType.MobileIdentity5GS{
+		Len:    12, // suci
+		Buffer: []uint8{0x01, 0x02, 0xf8, 0x39, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x47, 0x78},
+	}
+	ueSecurityCapability := ue.GetUESecurityCapability()
+	registrationRequest := nasTestpacket.GetRegistrationRequest(
+		nasMessage.RegistrationType5GSInitialRegistration, mobileIdentity5GS, nil, ueSecurityCapability, nil, nil, nil)
+	sendMsg, err = test.GetInitialUEMessage(ue.RanUeNgapId, registrationRequest, "")
+	assert.Nil(t, err)
+	_, err = conn.Write(sendMsg)
+	assert.Nil(t, err)
+
+	// receive NAS Authentication Request Msg
+	n, err = conn.Read(recvMsg)
+	assert.Nil(t, err)
+	ngapMsg, err := ngap.Decoder(recvMsg[:n])
+	assert.Nil(t, err)
+
+	// Calculate for RES*
+	nasPdu := test.GetNasPdu(ue, ngapMsg.InitiatingMessage.Value.DownlinkNASTransport)
+	require.NotNil(t, nasPdu)
+	require.NotNil(t, nasPdu.GmmMessage, "GMM message is nil")
+	require.Equal(t, nasPdu.GmmHeader.GetMessageType(), nas.MsgTypeAuthenticationRequest,
+		"Received wrong GMM message. Expected Authentication Request.")
+	rand := nasPdu.AuthenticationRequest.GetRANDValue()
+	resStat := ue.DeriveRESstarAndSetKey(ue.AuthenticationSubs, rand[:], "5G:mnc093.mcc208.3gppnetwork.org")
+
+	// send NAS Authentication Response
+	pdu := nasTestpacket.GetAuthenticationResponse(resStat, "")
+	sendMsg, err = test.GetUplinkNASTransport(ue.AmfUeNgapId, ue.RanUeNgapId, pdu)
+	assert.Nil(t, err)
+	_, err = conn.Write(sendMsg)
+	assert.Nil(t, err)
+
+	// receive NAS Security Mode Command Msg
+	n, err = conn.Read(recvMsg)
+	assert.Nil(t, err)
+	ngapPdu, err := ngap.Decoder(recvMsg[:n])
+	require.Nil(t, err)
+	require.NotNil(t, ngapPdu)
+	nasPdu = test.GetNasPdu(ue, ngapPdu.InitiatingMessage.Value.DownlinkNASTransport)
+	require.NotNil(t, nasPdu)
+	require.NotNil(t, nasPdu.GmmMessage, "GMM message is nil")
+	require.Equal(t, nasPdu.GmmHeader.GetMessageType(), nas.MsgTypeSecurityModeCommand,
+		"Received wrong GMM message. Expected Security Mode Command.")
+
+	// send NAS Security Mode Complete Msg
+	registrationRequestWith5GMM := nasTestpacket.GetRegistrationRequest(nasMessage.RegistrationType5GSInitialRegistration,
+		mobileIdentity5GS, nil, ueSecurityCapability, ue.Get5GMMCapability(), nil, nil)
+	pdu = nasTestpacket.GetSecurityModeComplete(registrationRequestWith5GMM)
+	pdu, err = test.EncodeNasPduWithSecurity(ue, pdu, nas.SecurityHeaderTypeIntegrityProtectedAndCipheredWithNew5gNasSecurityContext, true, true)
+	assert.Nil(t, err)
+	sendMsg, err = test.GetUplinkNASTransport(ue.AmfUeNgapId, ue.RanUeNgapId, pdu)
+	assert.Nil(t, err)
+	_, err = conn.Write(sendMsg)
+	assert.Nil(t, err)
+
+	// receive ngap Initial Context Setup Request Msg
+	n, err = conn.Read(recvMsg)
+	assert.Nil(t, err)
+	_, err = ngap.Decoder(recvMsg[:n])
+	assert.Nil(t, err)
+
+	// send ngap Initial Context Setup Response Msg
+	sendMsg, err = test.GetInitialContextSetupResponse(ue.AmfUeNgapId, ue.RanUeNgapId)
+	assert.Nil(t, err)
+	_, err = conn.Write(sendMsg)
+	assert.Nil(t, err)
+
+	// send NAS Registration Complete Msg
+	pdu = nasTestpacket.GetRegistrationComplete(nil)
+	pdu, err = test.EncodeNasPduWithSecurity(ue, pdu, nas.SecurityHeaderTypeIntegrityProtectedAndCiphered, true, false)
+	assert.Nil(t, err)
+	sendMsg, err = test.GetUplinkNASTransport(ue.AmfUeNgapId, ue.RanUeNgapId, pdu)
+	assert.Nil(t, err)
+	_, err = conn.Write(sendMsg)
+	assert.Nil(t, err)
+
+	// receive UE Configuration Update Command Msg
+	recvUeConfigUpdateCmd(t, recvMsg, conn)
+
+	// send PduSessionEstablishmentRequest Msg
+	sNssai := models.Snssai{
+		Sst: 1,
+		Sd:  "fedcba",
+	}
+	pdu = nasTestpacket.GetUlNasTransport_PduSessionEstablishmentRequest(10, nasMessage.ULNASTransportRequestTypeInitialRequest, "internet", &sNssai)
+	pdu, err = test.EncodeNasPduWithSecurity(ue, pdu, nas.SecurityHeaderTypeIntegrityProtectedAndCiphered, true, false)
+	assert.Nil(t, err)
+	sendMsg, err = test.GetUplinkNASTransport(ue.AmfUeNgapId, ue.RanUeNgapId, pdu)
+	assert.Nil(t, err)
+	_, err = conn.Write(sendMsg)
+	assert.Nil(t, err)
+
+	// receive 12. NGAP-PDU Session Resource Setup Request(DL nas transport((NAS msg-PDU session setup Accept)))
+	n, err = conn.Read(recvMsg)
+	assert.Nil(t, err)
+	_, err = ngap.Decoder(recvMsg[:n])
+	assert.Nil(t, err)
+
+	// send 14. NGAP-PDU Session Resource Setup Response
+	sendMsg, err = test.GetPDUSessionResourceSetupResponse(10, ue.AmfUeNgapId, ue.RanUeNgapId, ranN3Ipv4Addr)
+	assert.Nil(t, err)
+	_, err = conn.Write(sendMsg)
+	assert.Nil(t, err)
+
+	// Send Pdu Session Establishment Release Request
+	pdu = nasTestpacket.GetUlNasTransport_PduSessionReleaseRequest(10)
+	pdu, err = test.EncodeNasPduWithSecurity(ue, pdu, nas.SecurityHeaderTypeIntegrityProtectedAndCiphered, true, false)
+	assert.Nil(t, err)
+	sendMsg, err = test.GetUplinkNASTransport(ue.AmfUeNgapId, ue.RanUeNgapId, pdu)
+	assert.Nil(t, err)
+	_, err = conn.Write(sendMsg)
+	assert.Nil(t, err)
+
+	time.Sleep(1000 * time.Millisecond)
+	// send N2 Resource Release Ack(PDUSession Resource Release Response)
+	sendMsg, err = test.GetPDUSessionResourceReleaseResponse(ue.AmfUeNgapId, ue.RanUeNgapId)
+	assert.Nil(t, err)
+	_, err = conn.Write(sendMsg)
+	assert.Nil(t, err)
+
+	// wait 10 ms
+	time.Sleep(1000 * time.Millisecond)
+
+	//send N1 PDU Session Release Ack PDU session release complete
+	pdu = nasTestpacket.GetUlNasTransport_PduSessionReleaseComplete(10, nasMessage.ULNASTransportRequestTypeExistingPduSession, "internet", &sNssai)
+	pdu, err = test.EncodeNasPduWithSecurity(ue, pdu, nas.SecurityHeaderTypeIntegrityProtectedAndCiphered, true, false)
+	assert.Nil(t, err)
+	sendMsg, err = test.GetUplinkNASTransport(ue.AmfUeNgapId, ue.RanUeNgapId, pdu)
+	assert.Nil(t, err)
+	_, err = conn.Write(sendMsg)
+	assert.Nil(t, err)
+
+	// RAN connect to UPF
+	upfConn, err := test.ConnectToUpf(
+		ranN3Ipv4Addr, upfN3Ipv4Addr, 2151, 2152)
+	defer func() {
+		errConn := upfConn.Close()
+		if errConn != nil {
+			fmt.Printf("upfConn Close error in ueRanEmulator: %+v", errConn)
+		}
+	}()
+	fmt.Printf("++++++++++++++++++++++++++++++ [UERANEM] Connect to UPF successfully +++++++++++++++++++++++++++++++\n\n\n\n\n")
+
+	// infinite loop to send UDP with GTP
+	for {
+		fmt.Printf("[UERANEM] Send GTP packet to UPF\n\n\n\n")
+		if err = sendGTP(upfConn, "helloworld"); err != nil {
+			fmt.Printf("[UERANEM] Fail to send GTP packet!\n")
+			break
+		}
+		time.Sleep(1 * time.Second)
+	}
+
+	// ----------------------------------------------
+	// wait result
+	time.Sleep(1 * time.Second)
+
+	// delete test data
+	test.DelUeFromMongoDB(t, ue, servingPlmnId)
+
+	// terminate all NF
+	NfTerminate()
+}
+
+func sendGTP(conn *net.UDPConn, msg string) error {
+	// Hardcoded configuration
+	rawUdpSrcIP := "10.60.0.1"
+	// rawUdpDstIP := "10.10.0.21"
+	rawUdpDstIP := "8.8.8.8"
+	rawUdpsrcPort := uint16(30000)
+	rawUdpdstPort := uint16(30001)
+	teId := uint32(1)
+
+	pkt, err := test.BuildRawUdpIp(
+		rawUdpSrcIP, rawUdpDstIP, rawUdpsrcPort, rawUdpdstPort, []byte(msg))
+	if err != nil {
+		return err
+	}
+
+	// build GTPv1 header
+	gtpHdr, err := test.BuildGTPv1Header(false, 0, false, 0, false, 0, uint16(len(pkt)), teId)
+	if err != nil {
+		return err
+	}
+
+	tt := append(gtpHdr, pkt...)
+
+	// send to socket
+	_, err = conn.Write(tt)
+	return err
+}
---
+++ test_data_plane.sh
@@ -0,0 +1,163 @@
+#!/usr/bin/env bash
+
+# Check OS
+if [ -f /etc/os-release ]; then
+    # freedesktop.org and systemd
+    . /etc/os-release
+    OS=$NAME
+    VER=$VERSION_ID
+else
+    # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
+    OS=$(uname -s)
+    VER=$(uname -r)
+    echo "This Linux version is too old: $OS:$VER, we don't support!"
+    exit 1
+fi
+
+sudo -v
+if [ $? == 1 ]
+then
+    echo "Without root permission, you cannot run the test due to our test is using namespace"
+    exit 1
+fi
+
+while getopts 'o' OPT;
+do
+    case $OPT in
+        o) DUMP_NS=True;;
+    esac
+done
+shift $(($OPTIND - 1))
+
+TEST_POOL="All|TestPDUSessionSendData"
+if [[ ! "$1" =~ $TEST_POOL ]]
+then
+    echo "Usage: $0 [ ${TEST_POOL//|/ | } ]"
+    exit 1
+fi
+
+if [ $1 == "All" ]; then
+    echo "Running All Tests"
+    echo
+    mkdir -p testing_output
+    IFS='|' read -ra ADDR <<< "$TEST_POOL"
+        for i in "${ADDR[@]}"; do
+            if [ $i == "All" ]; then
+                continue
+            fi
+            echo "$i"
+            echo "    Output saved to testing_output/$i.log"
+            exec $(realpath $0) $i &> testing_output/$i.log &
+            wait
+            STATUS=$(grep -E "\-\-\-.*:" testing_output/$i.log)
+            if [ ! -z "$STATUS" ]; then
+                echo "$STATUS" | while read -r a; do echo "    ${a:4}"; done
+            else
+                echo "    Failed"
+            fi
+            echo
+        done
+    exit 1
+fi
+
+GOPATH=$HOME/go
+if [ $OS == "Ubuntu" ]; then
+    GOROOT=/usr/local/go
+elif [ $OS == "Fedora" ]; then
+    GOROOT=/usr/lib/golang
+fi
+PATH=$PATH:$GOPATH/bin:$GOROOT/bin
+
+UPFNS="UPFns"
+EXEC_UPFNS="sudo -E ip netns exec ${UPFNS}"
+
+export GIN_MODE=release
+
+function terminate()
+{
+    sleep 3
+    sudo killall -15 upf
+
+    if [ ${DUMP_NS} ]
+    then
+        # kill all tcpdump processes in the default network namespace
+        sudo killall tcpdump
+        sleep 1
+    fi
+
+    sudo ip link del veth0
+    sudo ip netns del ${UPFNS}
+    sudo ip addr del 10.60.0.1/32 dev lo
+
+    if [[ "$1" == "TestNon3GPP" ]]
+    then
+        if [ ${DUMP_NS} ]
+        then
+            cd .. && sudo ip xfrm state > ${PCAP_PATH}/NWu_SA_state.log
+        fi
+        sudo ip xfrm policy flush
+        sudo ip xfrm state flush
+        sudo ip netns del ${UENS}
+        removeN3iwfInterfaces
+        sudo ip link del veth2
+        sudo killall n3iwf
+        ps aux | grep test.test | awk '{print $2}' | xargs sudo kill -SIGUSR1
+    fi
+
+    if [[ "$1" == "TestMultiAmfRegistration" ]]
+    then
+        cd .. && ./force_kill.sh
+    fi
+
+    if [[ "$1" == "TestNasReroute" ]]
+    then
+        cd .. && ./force_kill.sh
+    fi
+
+    sleep 5
+}
+
+function handleSIGINT()
+{
+    echo -e "\033[41;37m Terminating due to SIGINT ... \033[0m"
+    terminate $1
+}
+
+trap handleSIGINT SIGINT
+
+
+
+
+# Setup network namespace
+sudo ip netns add ${UPFNS}
+
+sudo ip link add veth0 type veth peer name veth1
+sudo ip link set veth0 up
+sudo ip addr add 10.60.0.1 dev lo
+sudo ip addr add 10.200.200.1/24 dev veth0
+sudo ip addr add 10.200.200.2/24 dev veth0
+
+sudo ip link set veth1 netns ${UPFNS}
+
+${EXEC_UPFNS} ip link set lo up
+${EXEC_UPFNS} ip link set veth1 up
+${EXEC_UPFNS} ip addr add 10.60.0.101 dev lo
+${EXEC_UPFNS} ip addr add 10.200.200.101/24 dev veth1
+${EXEC_UPFNS} ip addr add 10.200.200.102/24 dev veth1
+
+if [ ${DUMP_NS} ]
+then
+    PCAP_PATH=testpcap
+    mkdir -p ${PCAP_PATH}
+    ${EXEC_UPFNS} tcpdump -U -i any -w ${PCAP_PATH}/${UPFNS}.pcap &
+    sudo -E tcpdump -U -i lo -w ${PCAP_PATH}/default_ns.pcap &
+fi
+
+${EXEC_UPFNS} ./bin/upf -c ./config/upfcfg.test_data_plane.yaml &./
+sleep 2
+
+cd test
+$GOROOT/bin/go test -v -vet=off -run "^$1$" -args $2
+
+
+terminate $1
